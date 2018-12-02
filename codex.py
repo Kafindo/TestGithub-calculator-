ea7b822d7849accd62965b309e963d52a944ab85
kafindo <heritierkafindo@protonmail.com>
2018-12-02 09:36:29 -0500

    Driver the code by setting a background,title,configuration of the Gui also by using StringVar() a variable of  class we create an instance and we added a text box enty plus the grid

--- codex.py
+++ codex.py
@@ -31,10 +31,24 @@ def clear():
     expression = ""
     equation.set("")
 
+if __name__ == "__main__":
+    gui = Tk()
 
+    gui.configure(background="blue")
 
-gui = Tk()
+    gui.title("calculator")
 
+    gui.geometry("325x150")
 
+    equation = StringVar()
+
+    expression_field = Entry(gui, textvariable=equation)
+
+    expression_field.grid(columnspan=4, ipadx=70)
+
+    equation.set('enter your expression')
+
+    
+   
 
 gui.mainloop()
